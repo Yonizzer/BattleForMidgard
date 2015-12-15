97454d61cf9ba1aec3b2e89866fceb55f03c0202
Yonizzer <yoann_poli@hotmail.com>
2015-12-15 11:04:04 +0100

    Adding lib_battle.h

---
+++ Introduction/headers/lib_battle.h
@@ -0,0 +1,16 @@
+/*
+** lib_battle.h for lib_battle in /home/yoann/projet/battle_for_midgard/poli_y/Introduction/headers
+** 
+** Made by POLI Yoann
+** Login   <poli_y@etna-alternance.net>
+** 
+** Started on  Tue Dec 15 10:40:23 2015 POLI Yoann
+** Last update Tue Dec 15 10:41:53 2015 POLI Yoann
+*/
+
+#ifndef LIB_BATTLE_H_
+# define LIB_BATTLE_H_
+
+char	*readLine();
+
+#endif
