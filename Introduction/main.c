0d9822f96ca012bed12f901fa15c6e66a8032dad
Yonizzer <yoann_poli@hotmail.com>
2015-12-15 09:24:38 +0100

    Adding step 1

---
+++ Introduction/main.c
@@ -0,0 +1,18 @@
+/*
+** main.c for main in /home/yoann/projet/battle_for_midgard/poli_y
+** 
+** Made by POLI Yoann
+** Login   <poli_y@etna-alternance.net>
+** 
+** Started on  Mon Dec 14 19:52:15 2015 POLI Yoann
+** Last update Mon Dec 14 20:03:53 2015 POLI Yoann
+*/
+
+#include "stdio.h"
+#include "stdlib.h"
+
+int main()
+{
+  printf("Hello wORLDDDDD\n");;
+  return (0);
+}
