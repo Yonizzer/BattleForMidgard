d8b1d6c94427e2980ff120be9223fe276f1d5c31
Yonizzer <yoann_poli@hotmail.com>
2015-12-16 11:21:08 +0100

    Update headers
    lib_battle.h

--- Introduction/headers/lib_battle.h
+++ Introduction/headers/lib_battle.h
@@ -5,12 +5,38 @@
 ** Login   <poli_y@etna-alternance.net>
 ** 
 ** Started on  Tue Dec 15 10:40:23 2015 POLI Yoann
-** Last update Tue Dec 15 10:41:53 2015 POLI Yoann
+** Last update Wed Dec 16 11:09:36 2015 POLI Yoann
 */
 
 #ifndef LIB_BATTLE_H_
 # define LIB_BATTLE_H_
 
-char	*readLine();
+/*
+** Prototype Function for project 
+*/
 
-#endif
+char	*readLine();
+int	begin(char *name);
+int	parse_arg(int argc, char **argv);
+void	help();
+void	message_handler(int error);
+
+typedef struct	s_creature
+{
+  char		*name;
+  int		lvl;
+  int		pv;
+  int		pvmax;
+  int		pm;
+  int		pmmax;
+}		t_creature;
+
+typedef struct	s_info
+{
+  char		*name;
+  int		pv;
+  int		pvmax;
+  t_creature	*team;
+}		t_info;
+
+#endif /* !LIB_BATTLE_H_ */
